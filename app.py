96b4edce4232faca5abee874b60fbed16e5f1093
kabdellah <khalil.mktg@gmail.com>
2021-04-27 21:50:28 -0400

    created a detail page about each victim

--- app.py
+++ app.py
@@ -19,6 +19,14 @@ def index():
     object_list = get_csv()
     return render_template(template, object_list=object_list)
 
+@app.route('/<row_id>/')
+def detail(row_id):
+    template = 'detail.html'
+    object_list = get_csv()
+    for row in object_list:
+        if row['id'] == row_id:
+            return render_template(template, object=row)
+
 #Configure Flask to make a page at site's root URL
 if __name__ == '__main__':
     #fire up flask test server
