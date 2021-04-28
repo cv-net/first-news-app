a158f557a0dd12c1dbd6de7720e47c971b778824
kabdellah <khalil.mktg@gmail.com>
2021-04-27 21:21:21 -0400

    Created table

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__) 
 
+def get_csv():
+#open csv, save it to variable
+    csv_path = './static/la-riots-deaths.csv'
+    csv_file = open(csv_path, 'r')
+#csv module DictReader, parse csv and return list of dictionaries
+    csv_obj = csv.DictReader(csv_file)
+#convert csv object to a permanent list so it doesnt disappear on use
+    csv_list = list(csv_obj)
+    return csv_list
+
 @app.route('/')
 def index():
     template = 'index.html'
-    return render_template(template)
+    object_list = get_csv()
+    return render_template(template, object_list=object_list)
 
 #Configure Flask to make a page at site's root URL
 if __name__ == '__main__':
